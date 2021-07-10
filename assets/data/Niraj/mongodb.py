702cdf4569a100f2f752e7b6e4e29dcaabb16f96
MahatmaK <nirajkhatri21@gmail.com>
2021-07-10 13:33:32 -0400

    Updated word doc. Updated db

--- assets/data/Niraj/mongodb.py
+++ assets/data/Niraj/mongodb.py
@@ -1,10 +1,25 @@
 # Dependencies
 from flask import Flask, render_template, redirect, Markup
-# from flask_pymongo import PyMongo
+from flask_pymongo import PyMongo
+import pymongo
 
-# import pymongo
+app = Flask(__name__)
 
-import pandas as pd
+# Use flask_pymongo to set up mongo connection
+app.config["MONGO_URI"] = "mongodb://localhost:27017/election2020"
+mongo = PyMongo(app)
+
+# create route that rendrs index.html template
+@app.route("/")
+def index():
+    myclient = pymongo.MongoClient("mongodb:localhost:27017/")
+    mydb = myclient["election2020"]
+    mycol = mydb["president_county_candidate"]
+
+    county = mycol.find({"county",""},{ "county": 1})
+
+    return render_template("index.html", county=county)
+
 
 # conn = "mongodb://localhost:27017/" 
 # client = pymongo.MongClient(conn)
@@ -14,7 +29,7 @@ import pandas as pd
 
 
 # PULL IN CSV
-csv_path = "Niraj/president_county_candidate.csv"
+# csv_path = "Niraj/president_county_candidate.csv"
 
-election_df = pd.read_csv(csv_path, encoding="utf-8")
-election_df.head()
+# election_df = pd.read_csv(csv_path, encoding="utf-8")
+# election_df.head()
