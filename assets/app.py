b4a86166cbea932ce198fd87207dfd52f3b15fde
MahatmaK <nirajkhatri21@gmail.com>
2021-07-13 02:41:43 -0400

    Create heatmap of US 2020 Elections

--- assets/app.py
+++ assets/app.py
@@ -4,6 +4,8 @@ from flask_pymongo import PyMongo
 from bson.json_util import dumps
 import pymongo
 from flask_cors import CORS
+import pandas as pd
+import json
 
 app = Flask(__name__)
 
@@ -17,37 +19,121 @@ mongo = PyMongo(app)
 @app.route("/")
 def index():
     
+    # Create dictionary to for topojson state ids
+    state_ids = {"Alabama":"01",
+                 "Alaska":"02",
+                 "Arizona":"04",
+                 "Arkansas":"05",
+                 "California":"06",
+                 "Colorado":"08",
+                 "Connecticut": "09",
+                 "Delaware":"10",
+                 "District of Columbia":"11",
+                 "Florida":"12",
+                 "Georgia":"13",
+                 "Hawaii":"15",
+                 "Idaho": "16",
+                 "Illinois": "17",
+                 "Indiana": "18",
+                 "Iowa": "19",
+                 "Kansas":"20",
+                 "Kentucky":"21",
+                 "Louisiana":"22",
+                 "Maine":"23",
+                 "Maryland":"24" ,
+                 "Massachusetts":"25",
+                 "Michigan":"26",
+                 "Minnesota":"27",
+                 "Mississippi":"28",
+                 "Missouri": "29",
+                 "Montana":"30",
+                 "Nebraska":"31",
+                 "Nevada": "32",
+                 "New Hampshire":"33",
+                 "New Jersey": "34",
+                 "New Mexico": "35",
+                "New York":"36",
+                "North Carolina": "37",
+                "North Dakota": "38",
+                "Ohio":"39",
+                "Oklahoma": "40",
+                "Oregon": "41",
+                "Pennsylvania": "42",
+                "Rhode Island":"44",
+                "South Carolina":"45",
+                "South Dakota": "46",
+                "Tennessee":"47",
+                "Texas": "48",
+                 "Utah":"49",
+                 "Vermont":"50",
+                 "Virginia":"51",
+                 "Washington":"53",
+                 "West Virginia": "54",
+                 "Wisconsin":"55",
+                 "Wyoming": "56"}
 
+    # Pull data from Mongo
     candidate = mongo.db.pCandidate.find()
     state = mongo.db.pState.find()
     county = mongo.db.pCounty.find()
 
     all_data = [candidate, state, county]
 
-    return_data = []
+    return_list = []
 
     for dataset in all_data:
 
         list_cur = list(dataset)
-        print(list_cur)
         json_data = dumps(list_cur)
-        return_data.append(json_data)
+        return_list.append(json_data)
+    
+    # Pull only candidate data
+    candidate_data = return_list[0]
+    
+    # Convert string of json to a json file
+    candidate_json = json.loads(candidate_data)
+    
+    # Convert json file to data frame
+    df = pd.DataFrame.from_records(candidate_json)
+    
+    # Filter data by our two candidates (Biden and Trump)
+    candidate_df = df.loc[(df["candidate"] == "Joe Biden") | (df["candidate"] == "Donald Trump"),:]
+    
+    # Group data by states
+    condensed_df = candidate_df.groupby(["state","candidate"]).sum()
+    
+    # Calculate Democrat win percentage
+    i = 0         # 0 means Trump, 1 means Biden 
+    count = 0
+    percentDem= {}
+       
+    for index, row in condensed_df.iterrows():
+        if i == 0:
+            # Trump's vote count
+            count = count + row["total_votes"]
+            i = i + 1
+        else:
+            # Calculate count for state
+            count = count + row["total_votes"]
 
-    return (return_data[0])
+            # Calculate percent
+            percent = row["total_votes"] / count
 
-# @app.route("localhost:8000/scrape")
-# conn = "mongodb://localhost:27017/" 
-# client = pymongo.MongClient(conn)
+            # Store current state
+            state = row.name[0]
 
-# db = client.election_2020
+            # Get topojson id based on current state
+            state_id = state_ids.get(state)
+
+            # Add ID and percent democrat to dictionary
+            percentDem.update({state_id : percent})
+
+            i = 0
+            count = 0
 
 
+    return percentDem
 
-# PULL IN CSV
-# csv_path = "Niraj/president_county_candidate.csv"
-
-# election_df = pd.read_csv(csv_path, encoding="utf-8")
-# election_df.head()
 
 if __name__ == "__main__":
     app.run(debug=True)
--- assets/templates/js/starterCode.js
+++ assets/templates/js/starterCode.js
@@ -1,13 +1,12 @@
-//us = fetch("states-albers-10m.json").json()
 
 // Access flask app for election2020 data (from Mongo)
 d3.json("http://127.0.0.1:5000/").then(function(data) {
 
-  // Pull election data. Calculate Joe Biden's % votes compared to Donald Trump by state
-  
+  var percentData = Object.values(data);
 
+  console.log(data);
 
-  // Pull topojson data to build map
+  // Pull topojson data to build map (https://bl.ocks.org/mbostock/4090848)
   d3.json("https://unpkg.com/us-atlas@1/us/10m.json").then(function(us) {
 
     // Define width and and height of svg
@@ -29,106 +28,31 @@ d3.json("http://127.0.0.1:5000/").then(function(data) {
   var svg = d3.select("svg");
   var g = svg.append("g")
   
-  // Add the map
-  const states = g.append("g")
-      .attr("fill", "#444")
-      .attr("cursor", "pointer")
-    .selectAll("path")
-    .data(topojson.feature(us, us.objects.states).features)
-    .join("path")
-      // .on("click", clicked)
-      .attr("d", path);
-
-
-  // Build color scale
-  var myColor = d3.scaleLinear()
+  var color = d3.scaleLinear()
     .range(["red", "blue"])
     .domain([0,1]);
 
-  // g.append("path")
-  //   .attr("fill", "none")
-  //   .attr("stroke", "white")
-  //   .attr("stroke-linejoin", "round")
-  //   .attr("d", path(topojson.mesh(us, us.objects.states, (a, b) => a !== b)));
+  color.domain(d3.extent(_.toArray(percentData)));
+
+  // Add the map 
+  const states = g.append("g")
+      .attr("cursor", "pointer")
+      .style("fill", "green")
+    .selectAll("path")
+    .data(topojson.feature(us, us.objects.states).features)
+    .enter().append("path") 
+      // .on("click", clicked)
+      .attr("d", path)
+      .style("fill", function(d,i) {
+        var index = d.id;
+        var value = data[index];
+        return (value)? color(value) : "#AAA"; 
+      });
+
+  g.append("path")
+    .attr("fill", "none")
+    .attr("stroke", "white")
+    .attr("stroke-linejoin", "round")
+    .attr("d", path(topojson.mesh(us, us.objects.states, (a, b) => a !== b)))
   });
 });
-
-//   d3.json("js/states-albers-10m.json").then(function(us) {
-  
-//   console.log(us);
-
-//   const width = 975;
-//   const height = 610;
-
-//   const zoom = d3.zoom()
-//       .scaleExtent([1, 8])
-//       .on("zoom", zoomed);
-
-//   const svg = d3.select("#USmap")
-//     .append("svg")
-//     .attr("viewBox", [0, 0, width, height])
-//     .on("click", reset);
-  
-  
-  
-  
-  
-  // const g = svg.append("g");
-
-  // const states = g.append("g")
-  //   .attr("fill", "#444")
-  //   .attr("cursor", "pointer")
-  //   .selectAll("path")
-  //   .data(topojson.feature(us, us.objects.states).features)
-  //   .join("path")
-  //     .on("click", clicked)
-  //     .attr("d", path);
-  
-  // states.append("title")
-  //     .text(d => d.properties.name);
-
-  // g.append("path")
-  //     .attr("fill", "none")
-  //     .attr("stroke", "white")
-  //     .attr("stroke-linejoin", "round")
-  //     .attr("d", path(topojson.mesh(us, us.objects.states, (a, b) => a !== b)));
-
-  // svg.call(zoom);
-
-  // function reset() {
-  //   states.transition().style("fill", null);
-  //   svg.transition().duration(750).call(
-  //     zoom.transform,
-  //     d3.zoomIdentity,
-  //     d3.zoomTransform(svg.node()).invert([width / 2, height / 2])
-  //   );
-  // }
-
-  // function clicked(event, d) {
-  //   const [[x0, y0], [x1, y1]] = path.bounds(d);
-  //   event.stopPropagation();
-  //   states.transition().style("fill", null);
-  //   d3.select(this).transition().style("fill", "red");
-  //   svg.transition().duration(750).call(
-  //     zoom.transform,
-  //     d3.zoomIdentity
-  //       .translate(width / 2, height / 2)
-  //       .scale(Math.min(8, 0.9 / Math.max((x1 - x0) / width, (y1 - y0) / height)))
-  //       .translate(-(x0 + x1) / 2, -(y0 + y1) / 2),
-  //     d3.pointer(event, svg.node())
-  //   );
-  // }
-
-  // function zoomed(event) {
-  //   const {transform} = event;
-  //   g.attr("transform", transform);
-  //   g.attr("stroke-width", 1 / transform.k);
-  // }
-
-  // return svg.node();
-
-
-  // data.forEach(element => {
-  //   console.log(element);    
-  // });
-
