74153dbdb9e1ed8c9430ed0d3ef6c03c74b58c98
supraja2710 <kvsupraja2710@gmail.com>
2021-07-16 01:26:26 -0400

    Added new HTML page fpr tables, new Js page for pagination code, used grid.js library for tabulation and pagination

--- assets/app.py
+++ assets/app.py
@@ -9,75 +9,84 @@ import json
 
 app = Flask(__name__)
 
-CORS(app, support_credentials = True)
+CORS(app, support_credentials=True)
 
 # Use flask_pymongo to set up mongo connection
 app.config["MONGO_URI"] = "mongodb://localhost:27017/election2020"
 mongo = PyMongo(app)
 
-# create route that rendrs index.html template
-@app.route("/")
-def index():
-    
-    # Create dictionary to for topojson state ids
-    state_ids = {"Alabama":"01",
-                 "Alaska":"02",
-                 "Arizona":"04",
-                 "Arkansas":"05",
-                 "California":"06",
-                 "Colorado":"08",
-                 "Connecticut": "09",
-                 "Delaware":"10",
-                 "District of Columbia":"11",
-                 "Florida":"12",
-                 "Georgia":"13",
-                 "Hawaii":"15",
-                 "Idaho": "16",
-                 "Illinois": "17",
-                 "Indiana": "18",
-                 "Iowa": "19",
-                 "Kansas":"20",
-                 "Kentucky":"21",
-                 "Louisiana":"22",
-                 "Maine":"23",
-                 "Maryland":"24" ,
-                 "Massachusetts":"25",
-                 "Michigan":"26",
-                 "Minnesota":"27",
-                 "Mississippi":"28",
-                 "Missouri": "29",
-                 "Montana":"30",
-                 "Nebraska":"31",
-                 "Nevada": "32",
-                 "New Hampshire":"33",
-                 "New Jersey": "34",
-                 "New Mexico": "35",
-                "New York":"36",
-                "North Carolina": "37",
-                "North Dakota": "38",
-                "Ohio":"39",
-                "Oklahoma": "40",
-                "Oregon": "41",
-                "Pennsylvania": "42",
-                "Rhode Island":"44",
-                "South Carolina":"45",
-                "South Dakota": "46",
-                "Tennessee":"47",
-                "Texas": "48",
-                 "Utah":"49",
-                 "Vermont":"50",
-                 "Virginia":"51",
-                 "Washington":"53",
-                 "West Virginia": "54",
-                 "Wisconsin":"55",
-                 "Wyoming": "56"}
+# Code to get all data from the database
 
+
+def getData():
     # Pull data from Mongo
     candidate = mongo.db.pCandidate.find()
     state = mongo.db.pState.find()
     county = mongo.db.pCounty.find()
 
     all_data = [candidate, state, county]
+
+    return all_data
+
+# create route that rendrs index.html template
+
+
+@app.route("/")
+def index():
+
+    # Create dictionary to for topojson state ids
+    state_ids = {"Alabama": "01",
+                 "Alaska": "02",
+                 "Arizona": "04",
+                 "Arkansas": "05",
+                 "California": "06",
+                 "Colorado": "08",
+                 "Connecticut": "09",
+                 "Delaware": "10",
+                 "District of Columbia": "11",
+                 "Florida": "12",
+                 "Georgia": "13",
+                 "Hawaii": "15",
+                 "Idaho": "16",
+                 "Illinois": "17",
+                 "Indiana": "18",
+                 "Iowa": "19",
+                 "Kansas": "20",
+                 "Kentucky": "21",
+                 "Louisiana": "22",
+                 "Maine": "23",
+                 "Maryland": "24",
+                 "Massachusetts": "25",
+                 "Michigan": "26",
+                 "Minnesota": "27",
+                 "Mississippi": "28",
+                 "Missouri": "29",
+                 "Montana": "30",
+                 "Nebraska": "31",
+                 "Nevada": "32",
+                 "New Hampshire": "33",
+                 "New Jersey": "34",
+                 "New Mexico": "35",
+                 "New York": "36",
+                 "North Carolina": "37",
+                 "North Dakota": "38",
+                 "Ohio": "39",
+                 "Oklahoma": "40",
+                 "Oregon": "41",
+                 "Pennsylvania": "42",
+                 "Rhode Island": "44",
+                 "South Carolina": "45",
+                 "South Dakota": "46",
+                 "Tennessee": "47",
+                 "Texas": "48",
+                 "Utah": "49",
+                 "Vermont": "50",
+                 "Virginia": "51",
+                 "Washington": "53",
+                 "West Virginia": "54",
+                 "Wisconsin": "55",
+                 "Wyoming": "56"}
+    all_data = getData()
 
     return_list = []
 
@@ -86,27 +95,28 @@ def index():
         list_cur = list(dataset)
         json_data = dumps(list_cur)
         return_list.append(json_data)
-    
+
     # Pull only candidate data
     candidate_data = return_list[0]
-    
+
     # Convert string of json to a json file
     candidate_json = json.loads(candidate_data)
-    
+
     # Convert json file to data frame
     df = pd.DataFrame.from_records(candidate_json)
-    
+
     # Filter data by our two candidates (Biden and Trump)
-    candidate_df = df.loc[(df["candidate"] == "Joe Biden") | (df["candidate"] == "Donald Trump"),:]
-    
+    candidate_df = df.loc[(df["candidate"] == "Joe Biden") | (
+        df["candidate"] == "Donald Trump"), :]
+
     # Group data by states
-    condensed_df = candidate_df.groupby(["state","candidate"]).sum()
-    
+    condensed_df = candidate_df.groupby(["state", "candidate"]).sum()
+
     # Calculate Democrat win percentage
-    i = 0         # 0 means Trump, 1 means Biden 
+    i = 0         # 0 means Trump, 1 means Biden
     count = 0
-    percentDem= {}
-       
+    percentDem = {}
+
     for index, row in condensed_df.iterrows():
         if i == 0:
             # Trump's vote count
@@ -126,13 +136,18 @@ def index():
             state_id = state_ids.get(state)
 
             # Add ID and percent democrat to dictionary
-            percentDem.update({state_id : percent})
+            percentDem.update({state_id: percent})
 
             i = 0
             count = 0
 
-
     return percentDem
+
+
+@app.route("/getAllRecords/<tableNumber>")
+def getAllRecords(tableNumber):
+    all_data = getData()
+    return dumps(all_data[int(tableNumber)])
 
 
 if __name__ == "__main__":
--- assets/templates/js/starterCode.js
+++ assets/templates/js/starterCode.js
@@ -1,155 +1,162 @@
 // Access flask app for election2020 data (from Mongo)
-d3.json("http://127.0.0.1:5000/").then(function(data) {
-
-  var counties;
-  var percentData = Object.values(data);
+d3.json('http://127.0.0.1:5000/').then(function (data) {
+  var counties
+  var percentData = Object.values(data)
 
   // Pull topojson data to build map (https://bl.ocks.org/mbostock/4090848)
-  d3.json("https://unpkg.com/us-atlas@1/us/10m.json").then(function(us) {
-
+  d3.json('https://unpkg.com/us-atlas@1/us/10m.json').then(function (us) {
     // Define county and state data sets
-    var countyData = us.objects.counties;
+    var countyData = us.objects.counties
     var someData = topojson.feature(us, countyData).features
     console.log(someData)
-    var stateData = us.objects.states;
+    var stateData = us.objects.states
 
     // Define width and and height of svg
-    const width = 975;
-    const height = 610;
+    const width = 975
+    const height = 610
 
-    const zoom = d3.zoom()
-    .scaleExtent([1, 8])
-    .on("zoom", zoomed);
+    const zoom = d3
+      .zoom()
+      .scaleExtent([1, 8])
+      .on('zoom', zoomed)
 
     // Append svg and define attributes
-    var  usMap = d3.select("#USmap")
-      .append("svg")
-      .attr("width", width)
-      .attr("height", height)
-      .attr("stroke-linejoin", "round")
-      .attr("stroke-linecap", "round")
-      .attr("viewBox", [0, 0, width, height])
-      .on("click", reset);
+    var usMap = d3
+      .select('#USmap')
+      .append('svg')
+      .attr('width', width)
+      .attr('height', height)
+      .attr('stroke-linejoin', 'round')
+      .attr('stroke-linecap', 'round')
+      .attr('viewBox', [0, 0, width, height])
+      .on('click', reset)
 
     // Allow users to reset map if they click outside of the svg
-    var body = d3.select("body")
-      .on("click", reset);
+    var body = d3.select('body').on('click', reset)
 
     // Define path to use later
-    var path = d3.geoPath();
+    var path = d3.geoPath()
 
     // Select into svg and append the graph
-    var svg = d3.select("svg");
-    var g = svg.append("g")
-    
+    var svg = d3.select('svg')
+    var g = svg.append('g')
+
     // Define color
-    var color = d3.scaleLinear()
-      .range(["red", "blue"])
-      .domain([0,1]); 
+    var color = d3
+      .scaleLinear()
+      .range(['red', 'blue'])
+      .domain([0, 1])
 
-    color.domain(d3.extent(_.toArray(percentData)));
+    color.domain(d3.extent(_.toArray(percentData)))
 
-    var states;
+    var states
     addMap()
 
-      
+    // FUNCTION
 
-
-  // FUNCTION
-
-  function addMap() {
-    // Add the map 
-    states = g.append("g")
-        .attr("cursor", "pointer")
-        .attr("fill", "#444")
-        .classed("map", true)
-        .selectAll("path")
-          .data(topojson.feature(us, stateData).features)
-          .enter().append("path") 
-            .on("click", clicked)
-            .attr("d", path)
-            .style("fill", function(d) {
-              var index = d.id; // index of topojson states are missing numbers (e.g. there is no "03" state id)
-              var value = data[index];
-              return (value)? color(value) : "#AAA"; 
-        });
-
-    // Add white lines between states
-    g.append("path")
-    .attr("fill", "none")
-    .attr("stroke", "white")
-    .attr("stroke-linejoin", "round")
-    .attr("d", path(topojson.mesh(us, stateData, (a, b) => a !== b)))
-    .classed("counties", true);
-  }
-  
-  
-  // Function when user clicks on a state
-  function clicked(event, d) {
-    const [[x0, y0], [x1, y1]] = path.bounds(d);
-
-    event.stopPropagation();
-
-    states.transition()
-    .style("fill", null)
-    .selectAll("path")
-
-    counties = g.append("g").selectAll("path")
-      .data(topojson.feature(us, countyData).features)
-      .enter().append("path") 
-        .attr("d", path)
-        .attr("stroke", "black")
-        .style("fill", function(d) {
-          var index = d.id; // index of topojson states are missing numbers (e.g. there is no "03" state id)
-          console.log(index);
-          var value = data[index];
-          return (value)? color(value) : "green"; 
-    });
+    function addMap () {
+      // Add the map
+      states = g
+        .append('g')
+        .attr('cursor', 'pointer')
+        .attr('fill', '#444')
+        .classed('map', true)
+        .selectAll('path')
+        .data(topojson.feature(us, stateData).features)
+        .enter()
+        .append('path')
+        .on('click', clicked)
+        .attr('d', path)
+        .style('fill', function (d) {
+          var index = d.id // index of topojson states are missing numbers (e.g. there is no "03" state id)
+          var value = data[index]
+          return value ? color(value) : '#AAA'
+        })
 
       // Add white lines between states
-      g.append("path")
-      .attr("fill", "none")
-      .attr("stroke", "white")
-      .attr("stroke-linejoin", "round")
-      .attr("d", path(topojson.mesh(us, stateData, (a, b) => a !== b)));
+      g.append('path')
+        .attr('fill', 'none')
+        .attr('stroke', 'white')
+        .attr('stroke-linejoin', 'round')
+        .attr('d', path(topojson.mesh(us, stateData, (a, b) => a !== b)))
+        .classed('counties', true)
+    }
 
+    // Function when user clicks on a state
+    function clicked (event, d) {
+      const [[x0, y0], [x1, y1]] = path.bounds(d)
 
-    // d3.select(this)
-    //   .transition()
-    //   .style("fill", "black")
-    
-    svg.transition().duration(750).call(
-      zoom.transform,
-      d3.zoomIdentity
-        .translate(width / 2, height / 2)
-        .scale(Math.min(8, 0.9 / Math.max((x1 - x0) / width, (y1 - y0) / height)))
-        .translate(-(x0 + x1) / 2, -(y0 + y1) / 2),
-      d3.pointer(event, svg.node())
-    );
-  }
+      event.stopPropagation()
 
-  // Function adjust graph as you zoom in on it
-  function zoomed(event) {
-    const {transform} = event;
-    g.attr("transform", transform);
-    g.attr("stroke-width", 1 / transform.k);
-  }
+      states
+        .transition()
+        .style('fill', null)
+        .selectAll('path')
 
-  // Function to reset the page after user has click away from counties view
-  function reset() {
-    states.style("fill", function(d,i) {
-      var index = d.id;
-      var value = data[index];
-      return (value)? color(value) : "#AAA";
-    }); 
-    
-    svg.transition().duration(750).call(
-      zoom.transform,
-      d3.zoomIdentity,
-      d3.zoomTransform(svg.node()).invert([width / 2, height / 2])
-    );       
-  }
-  });
-});
+      counties = g
+        .append('g')
+        .selectAll('path')
+        .data(topojson.feature(us, countyData).features)
+        .enter()
+        .append('path')
+        .attr('d', path)
+        .attr('stroke', 'black')
+        .style('fill', function (d) {
+          var index = d.id // index of topojson states are missing numbers (e.g. there is no "03" state id)
+          console.log(index)
+          var value = data[index]
+          return value ? color(value) : 'green'
+        })
 
+      // Add white lines between states
+      g.append('path')
+        .attr('fill', 'none')
+        .attr('stroke', 'white')
+        .attr('stroke-linejoin', 'round')
+        .attr('d', path(topojson.mesh(us, stateData, (a, b) => a !== b)))
 
+      // d3.select(this)
+      //   .transition()
+      //   .style("fill", "black")
+
+      svg
+        .transition()
+        .duration(750)
+        .call(
+          zoom.transform,
+          d3.zoomIdentity
+            .translate(width / 2, height / 2)
+            .scale(
+              Math.min(8, 0.9 / Math.max((x1 - x0) / width, (y1 - y0) / height))
+            )
+            .translate(-(x0 + x1) / 2, -(y0 + y1) / 2),
+          d3.pointer(event, svg.node())
+        )
+    }
+
+    // Function adjust graph as you zoom in on it
+    function zoomed (event) {
+      const { transform } = event
+      g.attr('transform', transform)
+      g.attr('stroke-width', 1 / transform.k)
+    }
+
+    // Function to reset the page after user has click away from counties view
+    function reset () {
+      states.style('fill', function (d, i) {
+        var index = d.id
+        var value = data[index]
+        return value ? color(value) : '#AAA'
+      })
+
+      svg
+        .transition()
+        .duration(750)
+        .call(
+          zoom.transform,
+          d3.zoomIdentity,
+          d3.zoomTransform(svg.node()).invert([width / 2, height / 2])
+        )
+    }
+  })
+})
