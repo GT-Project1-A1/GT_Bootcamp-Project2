fb1f246d408cf767ab675bb0ad0896a570a69bd1
MahatmaK <nirajkhatri21@gmail.com>
2021-07-15 21:12:00 -0400

    Updated bad data file on president_county_candidate

--- assets/templates/js/starterCode.js
+++ assets/templates/js/starterCode.js
@@ -1,6 +1,7 @@
 // Access flask app for election2020 data (from Mongo)
 d3.json("http://127.0.0.1:5000/").then(function(data) {
 
+  var counties;
   var percentData = Object.values(data);
 
   // Pull topojson data to build map (https://bl.ocks.org/mbostock/4090848)
@@ -47,9 +48,18 @@ d3.json("http://127.0.0.1:5000/").then(function(data) {
       .domain([0,1]); 
 
     color.domain(d3.extent(_.toArray(percentData)));
-    
+
+    var states;
+    addMap()
+
+      
+
+
+  // FUNCTION
+
+  function addMap() {
     // Add the map 
-    const states = g.append("g")
+    states = g.append("g")
         .attr("cursor", "pointer")
         .attr("fill", "#444")
         .classed("map", true)
@@ -63,16 +73,18 @@ d3.json("http://127.0.0.1:5000/").then(function(data) {
               var value = data[index];
               return (value)? color(value) : "#AAA"; 
         });
-      
+
     // Add white lines between states
     g.append("path")
-      .attr("fill", "none")
-      .attr("stroke", "white")
-      .attr("stroke-linejoin", "round")
-      .attr("d", path(topojson.mesh(us, stateData, (a, b) => a !== b)));
-
-
-  // Functions 
+    .attr("fill", "none")
+    .attr("stroke", "white")
+    .attr("stroke-linejoin", "round")
+    .attr("d", path(topojson.mesh(us, stateData, (a, b) => a !== b)))
+    .classed("counties", true);
+  }
+  
+  
+  // Function when user clicks on a state
   function clicked(event, d) {
     const [[x0, y0], [x1, y1]] = path.bounds(d);
 
@@ -82,10 +94,11 @@ d3.json("http://127.0.0.1:5000/").then(function(data) {
     .style("fill", null)
     .selectAll("path")
 
-    g.append("g").selectAll("path")
+    counties = g.append("g").selectAll("path")
       .data(topojson.feature(us, countyData).features)
       .enter().append("path") 
         .attr("d", path)
+        .attr("stroke", "black")
         .style("fill", function(d) {
           var index = d.id; // index of topojson states are missing numbers (e.g. there is no "03" state id)
           console.log(index);
@@ -93,16 +106,17 @@ d3.json("http://127.0.0.1:5000/").then(function(data) {
           return (value)? color(value) : "green"; 
     });
 
+      // Add white lines between states
+      g.append("path")
+      .attr("fill", "none")
+      .attr("stroke", "white")
+      .attr("stroke-linejoin", "round")
+      .attr("d", path(topojson.mesh(us, stateData, (a, b) => a !== b)));
 
-    g.append("path")
-    .attr("fill", "none")
-    .attr("stroke", "white")
-    .attr("stroke-linejoin", "round")
-    .attr("d", path(topojson.mesh(us, stateData, (a, b) => a !== b)));
 
-    d3.select(this)
-      .transition()
-      .style("fill", "black")
+    // d3.select(this)
+    //   .transition()
+    //   .style("fill", "black")
     
     svg.transition().duration(750).call(
       zoom.transform,
@@ -114,12 +128,14 @@ d3.json("http://127.0.0.1:5000/").then(function(data) {
     );
   }
 
+  // Function adjust graph as you zoom in on it
   function zoomed(event) {
     const {transform} = event;
     g.attr("transform", transform);
     g.attr("stroke-width", 1 / transform.k);
   }
 
+  // Function to reset the page after user has click away from counties view
   function reset() {
     states.style("fill", function(d,i) {
       var index = d.id;
